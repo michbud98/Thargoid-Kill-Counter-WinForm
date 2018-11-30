e11501b827410d4a4e013d9a6258d6b40b86cf8a
Prorok9999 <swiftblade1982@gmail.com>
2018-11-30 23:13:32 +0100

    Revert "Finished new methods comments"
    This reverts commit 4eeea29185e54a35f0a9616a54ac94ccca6a4f79.

--- TKC/JSONClasses.cs
+++ TKC/JSONClasses.cs
@@ -229,7 +229,7 @@ namespace TKC
             try
             {
                 resetThargoidKills();
-                List<FileInfo> filesList = JSONReaderInstance.GetJournalsDirectoryFiles();
+                List<FileInfo> filesList = JSONReaderInstance.GetJournalsDirectory();
                 string path = "";
                 for (int i = 0; i < filesList.Count; i++)
                 {
@@ -265,7 +265,7 @@ namespace TKC
         /// Method which find all Elite dangerous Journals in selected directory
         /// </summary>
         /// <returns>List of Journals </returns>
-        private List<FileInfo> GetJournalsDirectoryFiles()
+        private List<FileInfo> GetJournalsDirectory()
         {
             try
             {
@@ -318,7 +318,7 @@ namespace TKC
         /// </summary>
         /// <param name="directoryPathInput"> - directory path </param>
         /// <returns>List of Journals</returns>
-        private List<FileInfo> GetJournalsDirectoryFiles(string directoryPathInput)
+        private List<FileInfo> GetJournalsDirectory(string directoryPathInput)
         {
             try
             {
@@ -366,10 +366,6 @@ namespace TKC
 
         }
 
-        /// <summary>
-        /// Shows folder browser so that user can select Journals directory to browse
-        /// </summary>
-        /// <returns>List of Journals</returns>
         private List<FileInfo> SelectDirectory()
         { 
             //If app cant find default ED log directory. Alerts user to select directory
@@ -377,12 +373,11 @@ namespace TKC
             FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                return GetJournalsDirectoryFiles(folderBrowserDialog1.SelectedPath);
+                return GetJournalsDirectory(folderBrowserDialog1.SelectedPath);
             }
             else
             {
-                //Failed to select directory
-                return GetJournalsDirectoryFiles("");
+                return GetJournalsDirectory("");
             }
         }
     }
