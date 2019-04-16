d59981c58a338047d413805fc62826577e9c43cc
Prorok9999 <swiftblade1982@gmail.com>
2019-04-17 00:18:26 +0200

    Final changes before pull request.

--- JSONReaderLibrary/JSONReaderSingleton.cs
+++ JSONReaderLibrary/JSONReaderSingleton.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
